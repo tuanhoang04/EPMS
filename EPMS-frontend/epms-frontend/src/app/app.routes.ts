ccfe283c2098e6f564dd4b721baad9d4ec934854
tuanhoang04 <hoangngoctuan2611@gmail.com>
2026-03-30 15:29:27 +0700

    fix folder structure

--- EPMS-frontend/epms-frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { Homepage } from './features/homepage/pages/homepage/homepage';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: Homepage
-  }
-];
