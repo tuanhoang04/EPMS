9012adf72521ab93aa29bc588368f71bf4ad9c78
tuanhoang04 <hoangngoctuan2611@gmail.com>
2026-04-17 19:30:12 +0700

    fix image viewing

--- EPMS-backend/src/main/java/xyz/tuanhoang04/EPMS/config/SecurityConfig.java
+++ EPMS-backend/src/main/java/xyz/tuanhoang04/EPMS/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/exam-history/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
